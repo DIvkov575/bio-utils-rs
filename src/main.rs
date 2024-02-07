41fb247c5f39fa65cf1ddcac04dd061baa702b50
Divkov575 <dmitriy@ivkov.net>
2024-02-07 13:04:15 -0500

    added concurrency to tranlation func

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,22 @@
 mod maps;
 use maps::*;
 
-use regex::{Captures, Regex};
-use std::collections::HashMap;
-use lazy_static::lazy_static;
-use std::hash::BuildHasherDefault;
-use fnv::FnvHasher;
+use std::thread::JoinHandle;
 use anyhow::Result;
 use rayon::iter::IntoParallelRefIterator;
 use rayon::iter::ParallelIterator;
 use rayon::prelude::*;
 
 
-
-
-// fn dna_to_pre_rna(dna_str: &str) {
-//     dna_str.chars().collect::<Vec<_>>().par_iter_mut().map(|i|
-//         *i = dna_rna_nucleotide_pairs.get(&i.to_string()).unwrap().to_ascii_lowercase().chars().collect::<Vec<char>>()[0]
-//     );
-//     // return dna_str;
-// }
-
-
 fn main() -> Result<()> {
-    let mut dna = "atgcatatgac".to_string();
+    // let mut dna = " U U U G U G G U U U U U G C G G U G C C G C U G G U U U C C U U U ".to_string();
+    let mut dna = " T A C T T G C T T C A C C T C G C G C C G T T A T T G C G T C C G ".to_string();
+    dna = dna.replace(" ", "");
 
-    let mut rna = "".to_string();
-    for slice in get_slices(&mut dna, 3) { rna += &dna_to_rna(slice); }
+    let mut slices = get_slices(&mut dna, 3).iter().map(|x| x.to_string()).collect();
+    let rna = dna_to_rna(slices);
 
-    rna_to_aa(&mut rna);
+    // rna_to_aa(&mut rna);
 
 
     Ok(())
@@ -44,29 +32,39 @@ fn rna_to_aa(slice: &mut str) -> Vec<String> {
 }
 
 
-fn dna_to_rna(slice: &mut str) -> String {
-    slice
-        .chars()
-        .map(|char|
-            dna_rna_nucleotide_pairs
-                .get(&char.to_string())
-                .unwrap()
-                .chars()
-                .collect::<Vec<_>>()[0])
-        .collect()
+fn dna_to_rna(slices: Vec<String>) -> String {
+    let mut rna = String::from("");
+
+    let mut threads: Vec<JoinHandle<String>> = Vec::new();
+    for slice in slices {
+        let handle = std::thread::spawn(move || {
+            slice.chars()
+                .map(|char|
+                    dna_rna_nucleotide_pairs
+                        .get(&char.to_string())
+                        .unwrap()
+                        .chars()
+                        .collect::<Vec<_>>()[0])
+                .collect()
+        });
+        threads.push(handle);
+    }
+    for handle in threads { rna += &handle.join().unwrap(); }
+
+
+    rna
 }
 
 
 fn get_slices(input: &mut str, n: usize) -> Vec<&mut str> {
     let input_len = input.len();
-    let n = 3usize;
     let mut slices: Vec<&mut str> = Vec::new();
 
     let mut main_block: &mut str = input;
     let mut branch: &mut str;
 
     for _ in 0..(n-1) {
-        (branch, main_block) = (main_block).split_at_mut(input_len /n);
+        (branch, main_block) = main_block.split_at_mut(input_len /n);
         slices.push(branch);
     }
     slices.push(main_block);
